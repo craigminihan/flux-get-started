c0bf5d1def2f569c17318e1e0d095534c23314ad
Craig Minihan <craig.minihan@avon.com>
2020-04-20 00:11:15 +0100

    Updated

---
+++ install.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+
+helm upgrade -i flux fluxcd/flux \
+--set git.url=git@github.com:craigminihan/flux-get-started \
+--namespace flux --set git.readonly=true
+
+helm upgrade -i helm-operator fluxcd/helm-operator \
+--set git.ssh.secretName=flux-git-deploy \
+--namespace flux --set helm.versions=v3
